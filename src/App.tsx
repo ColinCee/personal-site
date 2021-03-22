c54d16d5ee0ddfa200694c25684eed422a4678a0
Colin Cheung <ColinCee@users.noreply.github.com>
2021-03-22 19:33:26 +0000

    update react ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import styled from "styled-components";
+import ReactGA from "react-ga";
 import Header from "./components/Header";
 import Home from "./components/Home";
 
@@ -8,6 +9,10 @@ const Container = styled.div`
   height: 100vh;
 `;
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-F246GL9SJW");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <Container>
       <Header />
